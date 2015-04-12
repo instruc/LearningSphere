6d7d54cd15b816ceab8e2b64f7e49ef1fc5fca77
instruc <instruc@mailinator.com>
2015-04-12 10:05:58 -0400

    Added script to open forum posts saved locally.

---
+++ openforum.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from sys import argv
+from os import remove
+from bs4 import BeautifulSoup
+import webbrowser
+
+if len(argv) != 2:
+	raise SystemExit('SYNTAX: python openforum.py PATH')
+
+with open(argv[1], 'r') as f:
+	c = f.read()
+
+soup = BeautifulSoup(c)
+hrefs = []
+for anchor in soup('a'):
+	if anchor.string == "Discuss this topic":
+		hrefs.append(anchor['href'])
+
+for href in hrefs:
+	webbrowser.open(href,new=2)
+
+remove(argv[1])

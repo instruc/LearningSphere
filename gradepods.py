d8fa7f15c8c3242989c4bd3a71d7d3d2671f738e
instruc <instruc@mailinator.com>
2015-05-18 21:11:49 -0400

    okay, env was a bad idea

--- gradepods.py
+++ gradepods.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # mark Pods as read and concatenate comments
 
--- openforum.py
+++ openforum.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # open 'Discuss this topic' links
 
